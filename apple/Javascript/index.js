b4885262658a0dd57a4b68814a10f15f09d96a51
25LP4037 25LP4037 <25LP4037@pinecones-iMac.local>
2025-10-08 15:45:38 +0800

    ssu

--- apple/Javascript/index.js
+++ apple/Javascript/index.js
@@ -1,4 +1,30 @@
-const puppy = 10;
-const cat = 35;
+// const arr = [235, 23, 5, 36, 7547];
 
-console.log(puppy - cat);
+// let max = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+const arr = [44, 32, 42, 1, 34, 3, 8, 4, 69, 247, 12, 41, 3, 53];
+
+let maxIndex = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > arr[maxIndex]) {
+    maxIndex = i;
+  }
+}
+
+//let max = arr[0];
+
+//for (let i = 0; 1 < arr.length; i++) {
+//  max = arr[i];
+//}
+
+//console.log(max);
+
+
