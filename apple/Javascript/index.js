58fc2fba416e1f8f8a1a2e863822c79e79a29d80
25LP4037 25LP4037 <25LP4037@pinecones-iMac.local>
2025-10-09 15:22:48 +0800

    soliko

--- apple/Javascript/index.js
+++ apple/Javascript/index.js
@@ -9,16 +9,6 @@
 // }
 // console.log(max);
 
-const arr = [44, 32, 42, 1, 34, 3, 8, 4, 69, 247, 12, 41, 3, 53];
-
-let maxIndex = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > arr[maxIndex]) {
-    maxIndex = i;
-  }
-}
-
 //let max = arr[0];
 
 //for (let i = 0; 1 < arr.length; i++) {
@@ -27,4 +17,52 @@ for (let i = 0; i < arr.length; i++) {
 
 //console.log(max);
 
+//const arr = [44, 32, 452, 1, 34, 3, 8, 4, 69, 247, 12, 41, 3, 53];
 
+//let max = 0;
+
+//for (let i = 0; i < arr.length; i++) {
+//if (arr[i] > arr[max]) {
+//max = i;
+//}
+//}
+
+//console.log(max);
+
+//const arr = [23, 352, 32, 543, 654, 76, 8, 9, 64, 3];
+//let max = 0;
+//for (let i = 0; i < arr.length; i++) {
+//if (arr[i] > arr[max]) {
+//max = i;
+//}
+//}
+//console.log(max);
+
+//const arr = [23, 21, 3, 324, 43, 453, 54654, 21, 3, 44, 54];
+//let max = 0;
+//for (let i = 0; i < arr.length; i++) {
+//  if (arr[i] > arr[max]) {
+//    max = i;
+//  }
+//}
+//console.log(max);
+
+//const arr = [235, 23, 5, 36, 7547];
+
+//let max = arr[0];
+
+//for (let i = 1; i < arr.length; i++) {
+//  if (arr[i] > max) {
+//    max = arr[i];
+//}
+//}
+//console.log(max);
+
+//const arr = [23, 454, 657, 542, 4432, 776, 8, 65, 75, 82451127];
+//let max = arr[0];
+//for (let i = 1; i < arr.length; i++) {
+//  if (arr[i] > max) {
+//    max = arr[i];
+//  }
+//}
+//console.log(max);
